8966e4b50ced7c63577a64ff6cdc1e2febfe5a21
bielb2 <54358297+bielb2@users.noreply.github.com>
2021-03-13 19:23:50 -0300

    :sparkles: feat(Menu component)

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,35 +1,60 @@
 import React, { useState } from 'react';
+import { MdKeyboardArrowUp } from 'react-icons/md';
+import Button from '../Button';
+
 import { Container, ContentList, ContainerList } from './styles';
 
 const Menu: React.FC = () => {
   const [isActive, setIsActive] = useState(false);
 
   return (
-    <Container>
+    <Container isActive={isActive}>
       <div>
-        <h1>Institucional</h1>
-        <button type="button" onClick={() => setIsActive(!isActive)}>
-          ^^
-        </button>
+        <span>Institucional</span>
+        <Button className="rotate" onClick={() => setIsActive(!isActive)}>
+          <MdKeyboardArrowUp />
+        </Button>
       </div>
-      <ContentList>
-        <ul>
-          <li>Quem somos</li>
-          <li>Fale Conosco</li>
-          <li>Endereço</li>
-        </ul>
-      </ContentList>
+      {isActive && (
+        <ContentList>
+          <ul>
+            <li>
+              <a href="/">Quem somos</a>
+            </li>
+            <li>
+              <a href="/">Fale Conosco</a>
+            </li>
+            <li>
+              <a href="/">Endereços</a>
+            </li>
+          </ul>
+        </ContentList>
+      )}
       <ContainerList>
         <ul>
-          <li>Novos</li>
-          <li>Ofertas</li>
-          <li>Seminovos</li>
           <li>
-            Consórcio <span className="new">Novo</span>
+            <a href="/">Novos</a>
           </li>
-          <li>Vendas Direta</li>
-          <li>Lifestyle</li>
-          <li>Pós-vendas</li>
+          <li>
+            <a href="/">Ofertas</a>
+          </li>
+          <li>
+            <a href="/">Seminovos</a>
+          </li>
+          <li>
+            <a href="/">
+              Consórcio <span>Novo</span>
+            </a>
+          </li>
+          <li>
+            <a href="/">Vendas Direta</a>
+          </li>
+          <li>
+            <a href="/">Lifestyle</a>
+          </li>
+          <li>
+            <a href="/">Pós-vendas</a>
+          </li>
         </ul>
       </ContainerList>
     </Container>
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -1,5 +1,109 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
-export const Container = styled.div``;
-export const ContentList = styled.nav``;
-export const ContainerList = styled.nav``;
+interface ContainerProps {
+  isActive: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
+  background: var(--nav-color);
+  position: absolute;
+  width: 100%;
+
+  ul {
+    list-style-type: none;
+
+    li {
+      border-bottom: 2px solid var(--border-color);
+      margin: 8px;
+      padding-bottom: 12px;
+      transition: border-color 0.4s;
+
+      &:hover {
+        border-bottom-color: #b7b8b8;
+      }
+
+      a {
+        transition: color 0.2s;
+        &:hover {
+          color: ${shade(0.2, '#FFFFFF')};
+        }
+      }
+    }
+  }
+
+  div {
+    background: var(--bg-red-color);
+    height: 50px;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex: 1;
+
+    padding: 16px;
+
+    span {
+      font-weight: 500;
+    }
+
+    button {
+      color: #fff;
+      display: flex;
+
+      font-weight: 600;
+      font-size: 24px;
+
+      svg {
+        transform: ${({ isActive }) =>
+          isActive ? 'rotate(180deg)' : 'rotate(0deg)'};
+        transition: transform 0.3s linear;
+
+        path {
+          transition: color 0.2s;
+        }
+
+        &:hover {
+          color: ${shade(0.2, '#fff')};
+        }
+      }
+    }
+  }
+`;
+
+export const ContentList = styled.nav`
+  margin: 0 24px 0 48px;
+
+  li {
+    a {
+      color: var(--title-color);
+    }
+
+    & + :last-child {
+      margin-bottom: 16px;
+    }
+  }
+`;
+
+export const ContainerList = styled.nav`
+  margin: 0 24px;
+
+  a {
+    color: var(--titleSemi-color);
+
+    span {
+      background: var(--bg-red-color);
+      font-weight: 600;
+      font-size: 10px;
+
+      padding: 4px;
+      border-radius: 8px;
+
+      margin-left: 8px;
+    }
+  }
+
+  li:last-child {
+    border: 0;
+  }
+`;
